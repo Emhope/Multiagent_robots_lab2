27fe28c015557a12d77f4f745147f85937baff22
Emhope <michailopalev02@gmail.com>
2023-12-03 18:38:09 +0300

    trying to add load and save scene possibilities

--- scene_tools.py
+++ scene_tools.py
@@ -1,102 +1,85 @@
-import pygame
 import numpy as np
+import random
+import pickle
+from collections import namedtuple
+
+from scene_objects import Field, Object
+
+class scene(namedtuple):
+    field: Field
+    grid: tuple[int]
+    widht: int
+    height: int
+    fps: int
+
+class obj_params(namedtuple):
+    q: float
+    m:float
+    k:float
+    r:float
+    width:float
+    color: tuple[int]
 
 
-
-class Field:
-    def __init__(self, objects: None | list['Object'] = None) -> None:
-        if objects is None:
-            self.objects = []
-        else:
-            self.objects = objects
+def _add_objects(
+        width: int,
+        height: int,
+        robot_params: obj_params,
+        robots_count: int,
+        field: Field
+        ) -> list[Object]:
     
-    def __iter__(self):
-        return iter(self.objects)
-    
-    def append(self, obj: 'Object'):
-        self.objects.append(obj)
-    
-    def get_others(self, obj: 'Object'):
-        obj_index = self.objects.index(obj)
-        return self.objects[: obj_index] + self.objects[obj_index+1:]
-    
-    def draw(self, screen:pygame.display):
-        for obj in self.objects:
-            if obj.m < 10:
-                obj.draw(screen, force_line=True)
-            else:
-                obj.draw(screen, force_line=False)
-
-
-
-class Object:
-
-    def __init__(
-            self, q: float, 
-            m:float, 
-            k:float, 
-            pos: np.ndarray[np.float64], 
-            r:float, 
-            width:float, 
-            color: tuple[int], 
-            field: Field
-            ) -> None:
-        self.q = q
-        self.m = m
-        self.k = k
-        self.v = np.zeros((2,))
-        self.f = np.zeros((2,))
-        self.pos = pos
-        self.field = field
-        self.r = r
-        self.width = width
-        self.color = color
-    
-    def apply_force(self, dt):
-        if self.m > 20:
-            return
-        
-        force = np.zeros((2,))
-        for obj in self.field.get_others(self):
-            rad_vector = obj.pos - self.pos
-            f = abs(self.q * obj.q) / rad_vector ** 2 * np.sign(rad_vector)
-
-            if self.r > abs(rad_vector[0]):
-                f[0] *= -1
-            if self.r > abs(rad_vector[1]):
-                f[1] *= -1
-
-            if self.q > 0 and obj.q > 0 or self.q < 0 and obj.q < 0:
-                force -= f
-            else:
-                force += f
-        force *= 1 / self.k
-        acceleration = force / self.m
-        self.f = force # just for make possible to draw force line at any time
-        self.v += acceleration * dt
-        self.pos += self.v * dt
-        
-
-    def _draw_force_line(self, screen: pygame.display):
-        pygame.draw.line(
-            surface=screen,
-            color=self.color,
-            start_pos=self.pos,
-            end_pos=self.pos+self.f,
-            width=1,
+    return [
+        Object(
+            pos=np.array([random.randint(50, width-50), random.randint(50, height-50)], dtype=np.float64),
+            **robot_params
         )
+        for _ in range(robots_count)
+    ]
 
-    def draw(self, screen: pygame.display, force_line: bool = False):
-        pygame.draw.circle(surface=screen, 
-                            color=self.color, 
-                            center=self.pos,
-                            radius=self.r, 
-                            width=self.width,
-                            )
-        if force_line:
-            self._draw_force_line(screen)
+def _add_grid(
+        width: int,
+        height: int,
+        grid_point_params: obj_params,
+        grid: tuple[int],
+        ) -> list[Object]:
+    
+    height_frame = (height - height * 0.9) / 2
+    width_frame = (width - width * 0.9) / 2
+    return [
+            Object(
+                q=-100,
+                m=1000,
+                k=1000,
+                pos=np.array(
+                    [x, y],
+                    dtype=np.float64
+                ),
+                r=5,
+                width=4,
+                color=(0, 0, 255),
+                field=field
+            )
+            for y in range(int(height_frame), height - int(height_frame) + 2, int((height - 2 * height_frame) / grid[1]))
+        for x in range(int(width_frame), width - int(width_frame) + 2, int((width - 2 * width_frame) / grid[0]))
+    ]
 
+def _add_goals()
 
-        
-        
+def create_field(
+        width: int,
+        height: int,
+        robot_params: obj_params,
+        robots_count: int,
+        goal_params: obj_params,
+        goals_count: int,
+        grid_point_params: obj_params,        
+        grid: tuple[int],
+        ) -> scene:
+    
+    field = Field()
+    _add_robots()
+    _add_grid()
+
+    
 
